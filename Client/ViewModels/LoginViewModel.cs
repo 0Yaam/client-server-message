02f9942ddc2fa4a96bc34085734e6f65904c0c33
Dan <nguyenngoctruongdan@gmail.com>
2025-10-24 12:23:34 +0700

    restructuring: update

---
+++ Client/ViewModels/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿// Client/ViewModels/LoginViewModel.cs
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels;
+
+public partial class LoginViewModel : ObservableObject
+{
+    [ObservableProperty] private string email = "";
+    [ObservableProperty] private string password = "";
+
+    [RelayCommand]
+    private Task Login()
+    {
+        // TODO: gọi AuthService.LoginAsync(Email, Password)
+        // demo:
+        System.Diagnostics.Debug.WriteLine($"Login: {Email}/{Password}");
+        return Task.CompletedTask;
+    }
+
+    [RelayCommand]
+    private Task Google()
+    {
+        // TODO: OAuth flow (nếu có)
+        System.Diagnostics.Debug.WriteLine("Google login clicked");
+        return Task.CompletedTask;
+    }
+}
---
+++ Client/Views/LoginView.axaml.cs
@@ -0,0 +1,19 @@
+﻿// Client/Views/LoginView.axaml.cs
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client.Views;
+
+public partial class LoginView : Window
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+
+    // Tự viết hàm này để nạp XAML nếu source generator chưa tạo
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
